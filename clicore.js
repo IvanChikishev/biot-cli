208b4234424ec994bc89d76645b80aa0db318b1b
Ivan Remmelgas <ivanremmelgas@gmail.com>
2018-06-30 14:48:11 +0300

    console.absolute

--- clicore.js
+++ clicore.js
@@ -27,75 +27,75 @@ const stateSequence = [
 
 program.command('getWallets')
     .description('Getting list of wallets')
-    .action(async () => console.log(await biot.getWallets()));
+    .action(async () => console.absolute(await biot.getWallets()));
 
 program.command('getMyDeviceWallets')
     .description('Getting list of wallets')
-    .action(async () => console.log(await biot.getMyDeviceWallets()));
+    .action(async () => console.absolute(await biot.getMyDeviceWallets()));
 
 program.command('getAddressesInWallet <walledId>')
     .description('Getting list of addresses')
-    .action(async (walletId) => console.log(await biot.getAddressesInWallet(walletId)));
+    .action(async (walletId) => console.absolute(await biot.getAddressesInWallet(walletId)));
 
 program.command('createNewWallet')
     .description('Creating new wallet')
-    .action(async () => console.log(await biot.createNewWallet()));
+    .action(async () => console.absolute(await biot.createNewWallet()));
 
 program.command('createNewAddress <walletId>')
     .description('Creating new address')
-    .action(async (walletId) => console.log(await biot.createNewAddress(walletId)));
+    .action(async (walletId) => console.absolute(await biot.createNewAddress(walletId)));
 
 program.command('getWalletBalance <walletId>')
     .description('Getting balance of wallet')
-    .action(async (walledId) => console.log(await biot.getWalletBalance(walletId)));
+    .action(async (walledId) => console.absolute(await biot.getWalletBalance(walletId)));
 
 program.command('getAddressBalance <address>')
     .description('Getting list of wallets')
-    .action(async (address) => console.log(await biot.getAddressBalance(address)));
+    .action(async (address) => console.absolute(await biot.getAddressBalance(address)));
 
 program.command('sendPaymentFromWallet <options>')
     .description('Sending payment from wallet')
-    .action(async (params) => console.log(await biot.sendPaymentFromWallet(params)));
+    .action(async (params) => console.absolute(await biot.sendPaymentFromWallet(params)));
 
 program.command('sendPaymentFromWalletUseUnstableUnits <options>')
     .description('Sending payment from wallet use unstable units')
-    .action(async (options) => console.log(await biot.sendPaymentFromWalletUseUnstableUnits(options)));
+    .action(async (options) => console.absolute(await biot.sendPaymentFromWalletUseUnstableUnits(options)));
 
 program.command('getListTransactionsForWallet <walletId>')
     .description('Getting list of transactions by wallet.')
-    .action(async (walletId) => console.log(await biot.getListTransactionsForWallet(walletId)));
+    .action(async (walletId) => console.absolute(await biot.getListTransactionsForWallet(walletId)));
 
 program.command('getListTransactionsForAddress <walletId>')
     .description('Getting list of transactions by address')
-    .action(async (walletId) => console.log(await biot.getListTransactionsForAddress(walletId)));
+    .action(async (walletId) => console.absolute(await biot.getListTransactionsForAddress(walletId)));
 
 program.command('myAddressInfo <address>')
     .description('Getting address info')
-    .action(async (address) => console.log(await biot.myAddressInfo(address)));
+    .action(async (address) => console.absolute(await biot.myAddressInfo(address)));
 
 program.command('signDevicePrivateKey <hash>')
     .description('Signing device private key')
-    .action(async (hash) => console.log(await biot.myAddressInfo(hash)));
+    .action(async (hash) => console.absolute(await biot.myAddressInfo(hash)));
 
 program.command('signWithAddress <account> <isChange> <addrressIndex> <hash>')
     .description('Signing with address')
-    .action(async (account, is_change, address_index, hash) => console.log(await biot.signWithAddress(account, is_change, address_index, hash)));
+    .action(async (account, is_change, address_index, hash) => console.absolute(await biot.signWithAddress(account, is_change, address_index, hash)));
 
 program.command('verifySign <hash> <b64Sig> <b64PubKey>')
     .description('Sign verification')
-    .action(async (hash, b64_sig, b64_pub_key) => console.log(await biot.verifySign(hash, b64_sig, b64_pub_key)));
+    .action(async (hash, b64_sig, b64_pub_key) => console.absolute(await biot.verifySign(hash, b64_sig, b64_pub_key)));
 
 program.command('addCorrespondent <code>')
     .description('Add a correspondent')
-    .action(async (code) => console.log(await biot.addCorrespondent(code)));
+    .action(async (code) => console.absolute(await biot.addCorrespondent(code)));
 
 program.command('removeCorrespondent <deviceAddress>')
     .description('Remove a correspondent')
-    .action(async (address) => console.log(await biot.removeCorrespondent(address)));
+    .action(async (address) => console.absolute(await biot.removeCorrespondent(address)));
 
 program.command('listCorrespondents')
     .description('List of correspondents')
-    .action(async () => console.log(await biot.removeCorrespondent()));
+    .action(async () => console.absolute(await biot.removeCorrespondent()));
 
 program.command('initSocketRpc <host> <port>')
     .description('initializes the rpc socket')
@@ -104,7 +104,7 @@ program.command('initSocketRpc <host> <port>')
         let service = socket.createServer(stateSequence);
         service.http().listen(port, host);
 
-        console.log("Server is running");
+        console.absolute("Server is running");
     });
 
 program.command('initSocketWs <host> <port>')
@@ -114,14 +114,16 @@ program.command('initSocketWs <host> <port>')
         let service = net.init(net.bind(stateSequence),
             { host: host, port: port });
 
-        console.log("Server is running");
+        console.absolute("Server is running");
     });
 
 
 async function Start() {
     await biot.init('test');
+    console.absolute = new console.Console(process.stdout, process.stderr).log;
+    
     program.parse(process.argv);
     return 'Ok';
 }
 
-Start().then(console.log).catch(console.error);
+Start().then(console.absolute).catch(console.error);
